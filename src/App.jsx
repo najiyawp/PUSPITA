70a5db0824eb96e03cb9c47ce5fadc122a8ddcb4
najiyawp <najiyarizqi70@gmail.com>
2026-01-28 10:08:19 +0700

    cart fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,20 @@ import Signup from "./components/Signup.jsx";
 import ProfilePage from "./components/ProfilePage.jsx";
 import EditProfilePage from "./components/EditProfilePage.jsx";
 
-// Komponen Admin yang diimpor dari folder src/components/admin
+// Komponen Admin
 import AdminLayout from "./components/admin/AdminLayout.jsx";
-import AdminRoute from "./components/admin/AdminRoute.jsx"; // Digunakan sebagai wrapper/Protected Route
 import AdminDashboard from "./components/admin/AdminDashboard.jsx";
 import AddProduct from "./components/admin/AddProduct.jsx";
 import ManageProducts from "./components/admin/ManageProducts.jsx";
 import EditProductPage from "./components/admin/EditProduct.jsx";
 import ManageOrders from "./components/admin/ManageOrders.jsx";
 import ManageUser from "./components/admin/ManageUser.jsx";
-// AboutUs.jsx sudah diimpor sebagai About
 
 function App() {
   return (
     <CartProvider>
       <Routes>
-        {/* Rute Halaman Utama */}
+        {/* --- RUTE PELANGGAN (USER) --- */}
         <Route
           path="/"
           element={
@@ -50,34 +48,34 @@ function App() {
         />
         
         <Route path="/ragam" element={<Ragam />} />
-
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/profile/edit" element={<EditProfilePage />} />
 
+        {/* Rute Produk & Keranjang */}
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:category" element={<ProductsPage />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
 
+        {/* Rute Pembayaran */}
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/payment/qris" element={<QRISPage />} />
         <Route path="/payment/success" element={<SuccessPage />} />
         <Route path="/order-tracking" element={<OrderTrackingPage />} />
 
-
-       <Route path="/admin" element={<AdminLayout />}>
-
-        <Route path="dashboard" element={<AdminDashboard />} />
-        <Route path="users" element={<ManageUser />} />
-        <Route path="products/new" element={<AddProduct />} />
-        <Route path="products/manage" element={<ManageProducts />} />
-        <Route path="/admin/products/edit/:productId" element={<EditProductPage />} />
-        <Route path="orders" element={<ManageOrders />} />
-
-      </Route>
+        {/* --- RUTE ADMIN (NESTED ROUTES) --- */}
+        {/* Kita tidak menggunakan '/' di depan path anak agar otomatis mengikuti parent-nya (/admin) */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="users" element={<ManageUser />} />
+          <Route path="products/new" element={<AddProduct />} />
+          <Route path="products/manage" element={<ManageProducts />} />
+          <Route path="products/edit/:productId" element={<EditProductPage />} />
+          <Route path="orders" element={<ManageOrders />} />
+        </Route>
         
       </Routes>
     </CartProvider>
